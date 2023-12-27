27702f399a1003d1a52b0b3a6426cb842aeacb05
lone_druid <enikita332@gmail.com>
2023-12-28 01:46:39 +0300

    add text input handle usage to demo

--- dcltui/__main__.py
+++ dcltui/__main__.py
@@ -3,6 +3,7 @@ from .common import (
     double_lined_box_component,
     text_input,
     wrap,
+    right_pad,
 )
 from .renderer import renderer
 from .dcl_types import Component
@@ -16,7 +17,7 @@ def main() -> None:
     simple_box = lambda x, y: double_lined_box_component(
         lambda ts: ((yst, yst), (3 + x, 4 + y))
     )
-    components: list[Component] = (
+    layout_components: list[Component] = (
         [double_lined_full_component]
         + [simple_box((xst * x), (yst * y)) for x in range(3) for y in range(3)]
         + [
@@ -31,12 +32,18 @@ def main() -> None:
             for j in range(3)
         ]
         + [
-            text_in := text_input((3, 2), "Input: ", 20, lambda s: 0)
+            text_in := text_input((3, 2), "Input: ", 20, lambda s: mut_render()) # lazy evaluation -> can use stuff defined later
             # dont forget to add incase you re-render with clear = True to not lose the input stuff
         ]
     )
-    render = renderer(components)
-    render(True)
+
+    layout_render = renderer(layout_components)
+
+    mut_comp = lambda ts, z: (right_pad(text_in.text, 20), (50, 2))
+    mut_render = renderer([mut_comp])
+
+    layout_render(True)
+
 
 
 if __name__ == "__main__":
