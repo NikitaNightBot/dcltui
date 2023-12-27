2ae33715d89991eb10b3965c99a60d64ddd4fa82
lone_druid <enikita332@gmail.com>
2023-12-28 00:06:15 +0300

    text + color in demo

--- dcltui/__main__.py
+++ dcltui/__main__.py
@@ -3,12 +3,25 @@ from .renderer import renderer
 from .dcl_types import Component
 
 def main() -> None:
-    simple_box=lambda x,y:double_lined_box_component(lambda ts:((9,9),(3+x,2+y)))
-    components: list[Component] = [double_lined_full_component] + [
-        simple_box((81*x),(9*y))
-        for x in range(3)
-        for y in range(3)
-    ]
+    xst = 20
+    yst = 10
+
+    simple_box=lambda x,y:double_lined_box_component(lambda ts:((yst,yst),(3+x,2+y)))
+    components: list[Component] = (
+        [
+            double_lined_full_component
+        ] + 
+        [
+            simple_box((xst*x),(yst*y))
+            for x in range(3)
+            for y in range(3)
+        ] + 
+        [
+            (lambda ts, z, text=text, i=i, j=j: (text, (4+(xst*i), 3+(yst*j))))
+            for i, text in enumerate(["\x1B[38;5;196mhey\n... hi\ni\nguess\x1B[0m", "test\nsome\nmulti\nlines", "damn\nthats\ncrazy"])
+            for j in range(3)
+        ]
+    )
     render = renderer(components)
     render(True)
 
