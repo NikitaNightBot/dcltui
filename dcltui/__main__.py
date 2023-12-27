c771a7e36b29f515354a11dc31cee76413ea1310
lone_druid <enikita332@gmail.com>
2023-12-28 01:50:02 +0300

    un-magic the numbers

--- dcltui/__main__.py
+++ dcltui/__main__.py
@@ -13,15 +13,25 @@ from .dcl_types import Component
 def main() -> None:
     xst = 20
     yst = 10
+    box_x_offset = 3
+    boy_y_offset = 4
+    count = 3
+
+    input_pos = (3, 2)
 
     simple_box = lambda x, y: double_lined_box_component(
-        lambda ts: ((yst, yst), (3 + x, 4 + y))
+        lambda ts: ((yst, yst), (box_x_offset + x, boy_y_offset + y))
     )
     layout_components: list[Component] = (
         [double_lined_full_component]
-        + [simple_box((xst * x), (yst * y)) for x in range(3) for y in range(3)]
+        + [simple_box((xst * x), (yst * y)) for x in range(count) for y in range(count)]
         + [
-            (lambda ts, z, text=text, i=i, j=j: (text, (4 + (xst * i), 5 + (yst * j)))) # late binding quick fix
+            (
+                lambda ts, z, text=text, i=i, j=j: (
+                    text,
+                    (box_x_offset + 1 + (xst * i), boy_y_offset + 1 + (yst * j)),
+                )
+            )  # late binding quick fix
             for i, text in enumerate(
                 [
                     "\x1B[38;5;196mhey\n... hi\ni\nguess\x1B[0m",
@@ -29,10 +39,12 @@ def main() -> None:
                     "damn\nthats\ncrazy",
                 ]
             )
-            for j in range(3)
+            for j in range(count)
         ]
         + [
-            text_in := text_input((3, 2), "Input: ", 20, lambda s: mut_render()) # lazy evaluation -> can use stuff defined later
+            text_in := text_input(
+                input_pos, "Input: ", 20, lambda s: mut_render()
+            )  # lazy evaluation -> can use stuff defined later
             # dont forget to add incase you re-render with clear = True to not lose the input stuff
         ]
     )
@@ -43,7 +55,6 @@ def main() -> None:
     mut_render = renderer([mut_comp])
 
     layout_render(True)
-
 
 
 if __name__ == "__main__":
