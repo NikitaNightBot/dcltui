ec149046b116b84eb0d84d815a3da014fba7169e
lone_druid <enikita332@gmail.com>
2023-12-28 01:12:46 +0300

    comment

--- dcltui/__main__.py
+++ dcltui/__main__.py
@@ -1,7 +1,7 @@
 from .common import double_lined_full_component, double_lined_box_component, text_input, done
 from .renderer import renderer
 from .dcl_types import Component
-
+from time import sleep
 def main() -> None:
     xst = 20
     yst = 10
@@ -20,12 +20,14 @@ def main() -> None:
             (lambda ts, z, text=text, i=i, j=j: (text, (4+(xst*i), 5+(yst*j))))
             for i, text in enumerate(["\x1B[38;5;196mhey\n... hi\ni\nguess\x1B[0m", "test\nsome\nmulti\nlines", "damn\nthats\ncrazy"])
             for j in range(3)
+        ] + 
+        [
+            text_in := text_input((3, 2), "Input: ", 20, lambda s:0) 
+            # dont forget to add incase you re-render with clear = True to not lose the input stuff
         ]
     )
     render = renderer(components)
     render(True)
-    
-    text_in = text_input((3, 2), "Input: ", 20, lambda s:0)
     done()
 
 if __name__ == "__main__":
