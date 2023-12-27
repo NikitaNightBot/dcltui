2c08dbf6448675690bfa09cc0863d6b9cba40a0a
lone_druid <enikita332@gmail.com>
2023-12-27 19:18:08 +0300

    fmt

--- dcltui/__main__.py
+++ dcltui/__main__.py
@@ -1,13 +1,13 @@
 from .common import double_lined_full_layout
 from .renderer import renderer
 
+
 def main() -> None:
-    render = renderer([
-        double_lined_full_layout
-    ])
+    render = renderer([double_lined_full_layout])
 
     render()
     input()
+
 
 if __name__ == "__main__":
     main()
--- dcltui/common.py
+++ dcltui/common.py
@@ -2,8 +2,10 @@ from .constants import *
 from os import terminal_size
 from .dcl_types import Element, Coords
 
+
 def cols_lines(size: terminal_size) -> Coords:  # columns, lines
     return (size.columns, size.lines)
+
 
 def double_lined_full_layout(term_size: terminal_size, z_idx: int) -> Element:
     cols, lines = cols_lines(term_size)
@@ -14,7 +16,7 @@ def double_lined_full_layout(term_size: terminal_size, z_idx: int) -> Element:
     hfill = d_horizontal * fcols
 
     mid_line = f"{d_vertical}{' '*fcols}{d_vertical}"
-    midlines = '\n'.join([mid_line]*flines)
+    midlines = "\n".join([mid_line] * flines)
 
     out = f"""\
 {d_top_left}{hfill}{d_top_right}
