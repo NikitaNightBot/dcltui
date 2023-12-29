b0fb8aefa5aec5a35319bba5333120e893b7e405
lone_druid <enikita332@gmail.com>
2023-12-29 13:48:02 +0300

    its alive???

--- dcltui/oops/dcl_types.py
+++ dcltui/oops/dcl_types.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
-from os import terminal_size
+from os import terminal_size, get_terminal_size
 from dataclasses import dataclass
-from typing import Self, Callable, TypeAlias, Type
+from typing import Self, Callable, TypeAlias
 from functools import wraps
+from textwrap import wrap
+from .text_utils import right_pad, write, flush
 
 
 @dataclass(slots=True, frozen=True)
@@ -25,6 +27,13 @@ class Vec2:
     __sub__ = bind(int.__sub__)
     __mul__ = bind(int.__mul__)
 
+    @staticmethod
+    def normalize(thing: tuple[int, int] | Vec2) -> Vec2:
+        if isinstance(thing, tuple):
+            return Vec2(thing[0], thing[1])
+        else:
+            return thing
+
 
 @dataclass(slots=True, frozen=True)
 class Element:
@@ -45,11 +54,46 @@ class Element:
         else:
             return [thing_s]
 
+    @staticmethod
+    def normalize_what(
+        thing: (
+            tuple[str, tuple[int, int]] | tuple[str, Vec2] | Element | list[Element]
+        )
+    ) -> list[Element]:
+        if isinstance(thing, (Element, list)):
+            elements = Element.normalize_list(thing)
+        elif isinstance(thing, tuple):
+            cords = thing[1]
+            real_cords = Vec2.normalize(cords)
+            element = Element.normalize_element((thing[0], real_cords))
+            elements = Element.normalize_list(element)
+        return elements
 
-Transform: TypeAlias = Callable[[terminal_size], Vec2]
-Output: TypeAlias = (
-    Callable[[terminal_size], Element] | Callable[[terminal_size], list[Element]]
+
+Transform: TypeAlias = (
+    Callable[[terminal_size], Vec2] | Callable[[terminal_size], tuple[int, int]]
 )
+Output: TypeAlias = (
+    Callable[[terminal_size], tuple[str, tuple[int, int]]]
+    | Callable[[terminal_size], tuple[str, Vec2]]
+    | Callable[[terminal_size], Element]
+    | Callable[[terminal_size], list[Element]]
+)
+
+
+@dataclass(slots=True, frozen=True)
+class Renderable:
+    element: Element
+    size: Vec2
+
+    def render(self: Renderable) -> None:
+        text, cords, s_x, s_y = self.element.text, self.element.cords, self.size.x, self.size.y
+        st_x, st_y = cords.x, cords.y
+        parts = wrap(text, s_x)
+        parts.extend([""]*(s_y-len(parts)))
+        for p_idx, part in enumerate(parts):
+            padded = right_pad(part, s_x)
+            write(f"\x1B[{st_y+1+p_idx};{st_x+1}H{padded}", False)
 
 
 @dataclass(slots=True)
@@ -61,9 +105,34 @@ class Component:
         def closure(ts: terminal_size, /) -> list[Element]:
             old = self.output(ts)
             new = new_output(ts)
-            return Element.normalize_list(old) + Element.normalize_list(new)
+            return Element.normalize_what(old) + Element.normalize_what(new)
 
         return type(self)(
             self.transform,
             closure,
         )
+
+    def __call__(self: Self) -> list[Renderable]:
+        ts = get_terminal_size()
+        size = Vec2.normalize(self.transform(ts))
+        return [
+            Renderable(element, size)
+            for element in Element.normalize_what(self.output(ts))
+        ]
+
+
+@dataclass(slots=True, frozen=True)
+class Renderer:
+    components: list[Component]
+
+    def __or__(self: Renderer, other: Renderer) -> Renderer:
+        return Renderer(self.components + other.components)
+
+    def __call__(self, clear: bool = False) -> None:
+        if clear is True:
+            write("\x1Bc")
+        for component in self.components:
+            renderables = component()
+            for renderable in renderables:
+                renderable.render()
+        flush()
---
+++ dcltui/oops/text_utils.py
@@ -0,0 +1,25 @@
+from sys import stdout
+from typing import Callable
+
+
+def flush():
+    stdout.flush()
+    
+
+def write(
+    text: str,
+    flush: bool = False,
+    w: Callable[[str], int | None] = stdout.write,
+    f: Callable[[], None] = stdout.flush,  # local names faster lookup
+) -> None:
+    w(text)
+    if flush:
+        f()
+
+
+def left_pad(text: str, size: int) -> str:
+    return ((" " * (size - len(text))) + text)[-size:]
+
+
+def right_pad(text: str, size: int) -> str:
+    return (text + (" " * (size - len(text))))[-size:]
